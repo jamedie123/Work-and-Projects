a0499914b3f8086e1f3cbde7e06726546bfe4972
Jacob Amedie <55558067+jamedie123@users.noreply.github.com>
2020-07-06 17:22:11 -0700

    Add files via upload

---
+++ Java-Script-Programs/Sec5/same_naive_solution.js
@@ -0,0 +1,20 @@
+/* Function called same accepts 2 arrays. Function should return True, if every value in the array has its corresponding value squared in the second array. The frequency of the values must be the same
+*/
+
+function same(arr1, arr2){
+    if(arr1.length !== arr2.length){
+        return false;
+    }
+    for(let i = 0; i < arr1.length; i++){
+        let correctIndex = arr2.indexOf(arr1[i] ** 2)
+        if(correctIndex === -1) {
+            return false;
+        }
+        console.log(arr2);
+        arr2.splice(correctIndex,1)
+    }
+    return true;
+}
+
+same([1,2,3,2], [9,1,4,4])
+
